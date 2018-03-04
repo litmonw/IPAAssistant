214b1573cbdc870bac0bdb3d37f48e08b5a24a10
litmonw <litmonw@163.com>
2018-03-04 12:48:08 +0800

    feature: 我的页面首页

---
+++ data/mine-list-data.js
@@ -0,0 +1,18 @@
+var data = [
+  {
+    title: '错题集',
+    url: 'wrongcollection/wrongcollection'
+  },
+  {
+    title: '意见反馈',
+    url: 'feedback/feedback'
+  },
+  {
+    title: '关于作者',
+    url: 'about/about'
+  }
+]
+
+module.exports = {
+  navList: data
+}
